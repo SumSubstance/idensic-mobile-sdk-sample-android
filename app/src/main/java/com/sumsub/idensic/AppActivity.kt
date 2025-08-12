d5cf9b10adf9eb36549721fc7ac5e7d1613ffe2d
Roman Zhilich <roman.zhilich@sumsub.com>
2025-08-12 19:19:40 +0400

    Fix Activity insets on Android 15+

--- app/src/main/java/com/sumsub/idensic/AppActivity.kt
+++ app/src/main/java/com/sumsub/idensic/AppActivity.kt
@@ -1,8 +1,13 @@
 package com.sumsub.idensic
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.google.android.gms.security.ProviderInstaller
 import timber.log.Timber
 
@@ -10,6 +15,7 @@ class AppActivity : AppCompatActivity(), ProviderInstaller.ProviderInstallListen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        applyEdgeToEdge()
         setContentView(R.layout.activity_app)
 
         ProviderInstaller.installIfNeededAsync(this, this)
@@ -21,5 +27,30 @@ class AppActivity : AppCompatActivity(), ProviderInstaller.ProviderInstallListen
 
     override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) {
         Timber.d("onProviderInstallFailed: errorCode=$errorCode")
+    }
+
+    private fun applyEdgeToEdge() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM) return
+
+        runCatching {
+            findViewById<View>(android.R.id.content)
+        }.onSuccess { view ->
+            ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowInsets ->
+                val insets = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemBars() or
+                            WindowInsetsCompat.Type.displayCutout() or
+                            WindowInsetsCompat.Type.ime()
+                )
+
+                view.updatePadding(
+                    left = insets.left,
+                    right = insets.right,
+                    top = insets.top,
+                    bottom = insets.bottom
+                )
+
+                WindowInsetsCompat.CONSUMED
+            }
+        }
     }
 }
